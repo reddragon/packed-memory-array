5daa2b2c7847742cd1d7fc76a046f6d0f2553ebd
Gaurav Menghani <gaurav.menghani@gmail.com>
2012-04-02 00:56:47 -0400

    More code

--- pma.cpp
+++ pma.cpp
@@ -1,10 +1,15 @@
 #include <cstdio>
 #include <vector>
 #include <cassert>
+#include <cmath>
 
-#define DEFAULT_C 2
-#define DEFAULT_T_L 0.5
-#define DEFAULT_T_0 1.0
+// WARNING: Do not change this.
+#define VAL_C 2
+#define VAL_T_L 0.5
+#define VAL_T_0 1.0
+// WARNING
+
+typedef unsigned int uint32;
 
 template <class E> 
 class PackedMemoryArray {
@@ -13,11 +18,14 @@ class PackedMemoryArray {
     // A bitmask to check if an element exists or not
     std::vector<int> exists_bitmask;
     // Upper thresholds for the level 0, and level l
-    double T_0, T_l;
+    double t_0, t_l;
     // The space requirement for n elements would be cn
+    // NOTE: c should be a power of 2 for easier math
     int c;
     // Number of levels = l+1
     int l;
+    // Number of elements in the PMA (the size)
+    uint32 s;
 
     public:
     PackedMemoryArray();
@@ -37,11 +45,13 @@ class PackedMemoryArray {
     // Does an element exist at position index?
     bool elem_exists_at(int index) const;
     // Capacity at level 'level'
-    int capacity_at(int level) const;
+    uint32 capacity_at(int level) const;
+    // Size of the PMA
+    uint32 size() const;
 
     private:
-    // Is the current PMA too full, with n_elems elements?
-    bool is_too_full(int n_elems) const;
+    // Is the current PMA too full?
+    bool is_too_full() const;
     // Is the 'level' level out of balance with n_elems elems?
     bool is_out_of_balance(int n_elems, int level) const;
     // Create a new PMA of size 'n_elems' 
@@ -55,7 +65,7 @@ class PackedMemoryArray {
 template <class E>
 double PackedMemoryArray<E>::upper_threshold_at(int level) const {
     assert(level <= l);
-    return T_0 - ((T_0 - T_l) * 1.0 * level) / l; 
+    return t_0 - ((t_0 - t_l) * 1.0 * level) / l; 
 }
 
 template <class E>
@@ -65,9 +75,9 @@ bool PackedMemoryArray<E>::elem_exists_at(int index) const {
 }
 
 template <class E>
-bool PackedMemoryArray<E>::is_too_full(int n_elems) const {
+bool PackedMemoryArray<E>::is_too_full() const {
     // TODO Will change when we get lower thresholds
-    return is_out_of_balance(n_elems, l);
+    return is_out_of_balance(s, l);
 }
 
 template <class E>
@@ -83,9 +93,35 @@ E PackedMemoryArray<E>::elem_at(int index) const {
 }
 
 template <class E>
-PackedMemoryArray<E>::PackedMemoryArray(E e) {
-    
+uint32 PackedMemoryArray<E>::size() const {
+    return s;
+}
+
+template <class E>
+uint32 PackedMemoryArray<E>::capacity_at(int level) const {
+    return 1<<level;
+}
+
+template <class E>
+PackedMemoryArray<E>::PackedMemoryArray(E e) : c(VAL_C), t_0(VAL_T_0), t_l(VAL_T_L) {
+    // Assert that c is a power of 2 and > 1
+    assert(c > 1 && !(c & (c-1)));
+    // Get the new store
+    store.resize(c*1);
+    store[0] = e; 
+    // One liner log2 since c is a power of 2 :-P
+    l = __builtin_popcount(c-1);
+    s = 1;
+    // Now assert that the upper thresholds are sane, and you do not go out of balance the very first time.
+    assert(!is_too_full());
+    // And we have set this thing in motion. Pray!
+}
+
+template <class E>
+PackedMemoryArray<E>::~PackedMemoryArray() {
 }
 
 int main() {
+    int e = 3;
+    PackedMemoryArray<int> pma(e);
 }
