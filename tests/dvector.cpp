7ed21ddf259ad058e6ff7f719a85c113eedaca56
Gaurav Menghani <gaurav.menghani@gmail.com>
2012-04-17 11:49:21 -0400

    Added time reporting

--- tests/dvector.cpp
+++ tests/dvector.cpp
@@ -1,14 +1,23 @@
 #include "dvector.hpp"
+#include <ctime>
 
 int
 main() {
     deamortized_vector<int> dvi;
-    // vector<int> dvi;
-    for (int i = 0; i < 50000000; ++i) {
-        dvi.push_back(i);
+    vector<int> vi;
+    for (int i = 0; i < 40000; ++i) {
+		clock_t start = clock();
+		for (int j = 0; j < 128; ++j) {
+			dvi.push_back(j);
+			//vi.push_back(j);
+		}
+		clock_t end = clock();
+		if (end - start + 1 > 1)
+			printf("%d\t%ld\n", i, end - start + 1);
     }
+    /*
     for (int i = 0; i < 100; ++i) {
         // printf("%3d, ", dvi[i]);
         assert(dvi[i] == i);
-    }
+    }*/
 }
