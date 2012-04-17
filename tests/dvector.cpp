11dd94339d58f69a99e0f7b40ead617f2cf07ecc
Gaurav Menghani <gaurav.menghani@gmail.com>
2012-04-17 12:26:40 -0400

    Now plotting i*128 instead of i on the x axis

--- tests/dvector.cpp
+++ tests/dvector.cpp
@@ -9,12 +9,12 @@ main() {
     for (int i = 0; i < 4096; ++i) {
 		t.start();
 		for (int j = 0; j < 128; ++j) {
-			//dvi.push_back(j);
-			vi.push_back(j);
+			dvi.push_back(j);
+			//vi.push_back(j);
 		}
 		//if (end - start + 1 > 1)
 		double d = t.stop();
-		printf("%d\t%lf\n", i, d);
+		printf("%d\t%lf\n", i<<7, d);
     }
     /*
     for (int i = 0; i < 100; ++i) {
--- tests/plot.plt
+++ tests/plot.plt
@@ -3,6 +3,7 @@ set ylabel "Time taken (in logscale)"
 set terminal png
 set output 'deamortised.png'
 set logscale y
+set logscale x 2
 plot 'deamortized.dat' with lines
 set terminal png
 set output 'normal.png'
