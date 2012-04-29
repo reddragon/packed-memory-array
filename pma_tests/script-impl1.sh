a99d6209af4ac52cbde168c11c376c77ef37c5f6
Gaurav Menghani <gaurav.menghani@gmail.com>
2012-04-29 23:50:00 +0000

    Timing scripts

---
+++ pma_tests/script-impl1.sh
@@ -0,0 +1,51 @@
+./impl1 hammer 10 >> impl1-hammer-results
+./impl1 hammer 100 >> impl1-hammer-results
+./impl1 hammer 1000 >> impl1-hammer-results
+./impl1 hammer 10000 >> impl1-hammer-results
+./impl1 hammer 100000 >> impl1-hammer-results
+./impl1 hammer 1000000 >> impl1-hammer-results
+./impl1 hammer 10000000 >> impl1-hammer-results
+./impl1 hammer 10000000 >> impl1-hammer-results
+
+./impl1 hammer 1024 >> impl1-hammer-results-pow2
+./impl1 hammer 2048 >> impl1-hammer-results-pow2
+./impl1 hammer 4096 >> impl1-hammer-results-pow2
+./impl1 hammer 8192 >> impl1-hammer-results-pow2
+./impl1 hammer 16384 >> impl1-hammer-results-pow2
+./impl1 hammer 32768 >> impl1-hammer-results-pow2
+./impl1 hammer 65536 >> impl1-hammer-results-pow2
+./impl1 hammer 65536 >> impl1-hammer-results-pow2
+./impl1 hammer 131072 >> impl1-hammer-results-pow2
+./impl1 hammer 262144 >> impl1-hammer-results-pow2
+./impl1 hammer 524288 >> impl1-hammer-results-pow2
+./impl1 hammer 1048576 >> impl1-hammer-results-pow2
+./impl1 hammer 2097152 >> impl1-hammer-results-pow2
+./impl1 hammer 4194304 >> impl1-hammer-results-pow2
+./impl1 hammer 8388608 >> impl1-hammer-results-pow2
+./impl1 hammer 16777216 >> impl1-hammer-results-pow2
+
+./impl1 random 10 >> impl1-random-results
+./impl1 random 100 >> impl1-random-results
+./impl1 random 1000 >> impl1-random-results
+./impl1 random 10000 >> impl1-random-results
+./impl1 random 100000 >> impl1-random-results
+./impl1 random 1000000 >> impl1-random-results
+./impl1 random 10000000 >> impl1-random-results
+
+./impl1 random 1024 >> impl1-random-results-pow2
+./impl1 random 2048 >> impl1-random-results-pow2
+./impl1 random 4096 >> impl1-random-results-pow2
+./impl1 random 8192 >> impl1-random-results-pow2
+./impl1 random 16384 >> impl1-random-results-pow2
+./impl1 random 32768 >> impl1-random-results-pow2
+./impl1 random 65536 >> impl1-random-results-pow2
+./impl1 random 65536 >> impl1-random-results-pow2
+./impl1 random 131072 >> impl1-random-results-pow2
+./impl1 random 262144 >> impl1-random-results-pow2
+./impl1 random 524288 >> impl1-random-results-pow2
+./impl1 random 1048576 >> impl1-random-results-pow2
+./impl1 random 2097152 >> impl1-random-results-pow2
+./impl1 random 4194304 >> impl1-random-results-pow2
+./impl1 random 8388608 >> impl1-random-results-pow2
+./impl1 random 16777216 >> impl1-random-results-pow2
+
---
+++ pma_tests/script-impl2.sh
@@ -0,0 +1,51 @@
+./impl2 hammer 10 >> impl1-hammer-results
+./impl2 hammer 100 >> impl1-hammer-results
+./impl2 hammer 1000 >> impl1-hammer-results
+./impl2 hammer 10000 >> impl1-hammer-results
+./impl2 hammer 100000 >> impl1-hammer-results
+./impl2 hammer 1000000 >> impl1-hammer-results
+./impl2 hammer 10000000 >> impl1-hammer-results
+./impl2 hammer 10000000 >> impl1-hammer-results
+
+./impl2 hammer 1024 >> impl1-hammer-results-pow2
+./impl2 hammer 2048 >> impl1-hammer-results-pow2
+./impl2 hammer 4096 >> impl1-hammer-results-pow2
+./impl2 hammer 8192 >> impl1-hammer-results-pow2
+./impl2 hammer 16384 >> impl1-hammer-results-pow2
+./impl2 hammer 32768 >> impl1-hammer-results-pow2
+./impl2 hammer 65536 >> impl1-hammer-results-pow2
+./impl2 hammer 65536 >> impl1-hammer-results-pow2
+./impl2 hammer 131072 >> impl1-hammer-results-pow2
+./impl2 hammer 262144 >> impl1-hammer-results-pow2
+./impl2 hammer 524288 >> impl1-hammer-results-pow2
+./impl2 hammer 1048576 >> impl1-hammer-results-pow2
+./impl2 hammer 2097152 >> impl1-hammer-results-pow2
+./impl2 hammer 4194304 >> impl1-hammer-results-pow2
+./impl2 hammer 8388608 >> impl1-hammer-results-pow2
+./impl2 hammer 16777216 >> impl1-hammer-results-pow2
+
+./impl2 random 10 >> impl1-random-results
+./impl2 random 100 >> impl1-random-results
+./impl2 random 1000 >> impl1-random-results
+./impl2 random 10000 >> impl1-random-results
+./impl2 random 100000 >> impl1-random-results
+./impl2 random 1000000 >> impl1-random-results
+./impl2 random 10000000 >> impl1-random-results
+
+./impl2 random 1024 >> impl1-random-results-pow2
+./impl2 random 2048 >> impl1-random-results-pow2
+./impl2 random 4096 >> impl1-random-results-pow2
+./impl2 random 8192 >> impl1-random-results-pow2
+./impl2 random 16384 >> impl1-random-results-pow2
+./impl2 random 32768 >> impl1-random-results-pow2
+./impl2 random 65536 >> impl1-random-results-pow2
+./impl2 random 65536 >> impl1-random-results-pow2
+./impl2 random 131072 >> impl1-random-results-pow2
+./impl2 random 262144 >> impl1-random-results-pow2
+./impl2 random 524288 >> impl1-random-results-pow2
+./impl2 random 1048576 >> impl1-random-results-pow2
+./impl2 random 2097152 >> impl1-random-results-pow2
+./impl2 random 4194304 >> impl1-random-results-pow2
+./impl2 random 8388608 >> impl1-random-results-pow2
+./impl2 random 16777216 >> impl1-random-results-pow2
+
