5ed39041c5d2be952f010c75eaa76981b877b85d
Gaurav Menghani <gaurav.menghani@gmail.com>
2012-04-29 17:01:38 -0400

    PMA Tests

---
+++ pma_tests/impl1.cpp
@@ -0,0 +1,500 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <cmath>
+#include "../include/timer.hpp"
+#include <iostream>
+#include <cstdio>
+
+// WARNING: Do not change this.
+#define VAL_C 2
+#define VAL_T_L 0.5
+#define VAL_T_0 1.0
+#define ELEM_EXISTS_AT(i) exists[i]
+#define OPTIMIZE 1 
+#define CAPACITY_AT(l) ((int)(segment_size<<l))
+// WARNING
+
+typedef unsigned int uint32;
+
+template <class E> 
+class PackedMemoryArray {
+    // The actual array
+    std::vector<E> store;
+    // A bitmask to check if an element exists or not
+    std::vector<bool> exists;
+    // Upper thresholds for the level 0, and level l
+    double t_0, t_l;
+    // The space requirement for n elements would be cn
+    // NOTE: c should be a power of 2 for easier math
+    int c;
+    // Number of levels = l+1
+    int l;
+    // Number of elements in the PMA (the size)
+    uint32 s;
+    // Segment size
+    // Basically round up log2(n) to a power of 2
+    int segment_size;
+    
+    public:
+    PackedMemoryArray();
+    PackedMemoryArray(E e);
+    PackedMemoryArray(std::vector<E> v);
+    ~PackedMemoryArray();
+    
+    long long total_moves;
+    int upper_bound_in_segment(E e, int v);
+    int upper_bound(E e);
+    // A generic insert
+    void insert_element(E e);
+    // Insert after the element elem
+    void insert_element_after(E e, E after, int pos = -1);
+    // Insert at index
+    void insert_element_at(E e, int index);
+    
+    // TODO Delete the element at index 'index'
+    //      Support this later.
+    //      bool delete_elem(int index);
+    void delete_element_at(int index);
+
+    // Return the element at index 'index'
+    E elem_at(int index) const;
+    // Does an element exist at position index?
+    bool elem_exists_at(int index) const;
+    // Find the location of element 'e'
+    int find(E e) const;
+    // Capacity at level 'level'
+    uint32 capacity_at(int level) const;
+    // Size of the PMA
+    uint32 size() const;
+    // Actual size of the store
+    uint32 store_size() const;
+    // Print the PMA
+    void print() const;
+
+    private:
+    // Is the current PMA too full?
+    bool is_too_full() const;
+    // Is the 'level' level out of balance with n_elems elems?
+    bool is_out_of_balance(int n_elems, int level) const;
+    // Expand (double up) the current PMA and insert element e
+    void expand_PMA(E e);
+    // Rebalance from the index 'index' at level 'level'
+    void rebalance(int index, int level);
+    // Rebalance from the index 'index' at level 'level', and insert element 'e'
+    void rebalance(int index, int level, E e);
+    // Return the threshold at 'level'
+    double upper_threshold_at(int level) const;
+    // Find the smallest interval encompassing index 'index' which is not out of balance
+    int smallest_interval_in_balance(int index, int * node_index, int * node_level) const;
+};
+
+template <class E>
+double PackedMemoryArray<E>::upper_threshold_at(int level) const {
+#ifndef OPTIMIZE
+    assert(level <= l);
+#endif
+    return t_0 - ((t_0 - t_l) * 1.0 * level) / l; 
+}
+
+template <class E>
+bool PackedMemoryArray<E>::elem_exists_at(int index) const {
+#ifndef OPTIMIZE
+    assert(index < (sizeof(int)*exists.size()));
+#endif
+    return (exists[index]);
+}
+
+template <class E>
+bool PackedMemoryArray<E>::is_too_full() const {
+    // TODO Will change when we get lower thresholds
+    return is_out_of_balance(s, l);
+}
+
+template <class E>
+bool PackedMemoryArray<E>::is_out_of_balance(int n_elems, int level) const {
+   // TODO Will change when we get lower thresholds
+   return ((int)floor(upper_threshold_at(level) * CAPACITY_AT(level)) < n_elems);
+}
+
+template <class E>
+E PackedMemoryArray<E>::elem_at(int index) const {
+#ifndef OPTIMIZE
+    assert(ELEM_EXISTS_AT(index));
+#endif
+    return store[index];
+}
+
+template <class E>
+uint32 PackedMemoryArray<E>::size() const {
+    return s;
+}
+
+template <class E>
+uint32 PackedMemoryArray<E>::store_size() const {
+    return (uint32)(store.size());
+}
+
+template <class E>
+uint32 PackedMemoryArray<E>::capacity_at(int level) const {
+    return segment_size << level;
+}
+
+template <class E>
+PackedMemoryArray<E>::PackedMemoryArray(E e) : t_0(VAL_T_0), t_l(VAL_T_L), c(VAL_C) {
+    // Assert that c is a power of 2 and > 1
+#ifndef OPTIMIZE
+    assert(c > 1 && !(c & (c-1)));
+#endif
+    s = 0;
+    // Get the new store
+    store.resize(c*1);
+    // Resize the bitmask as well
+    exists.resize((size_t)ceil(c));
+    insert_element_at(e, 0);
+    
+    // One liner log2 since c is a power of 2 :-P
+    int log2n = __builtin_popcount(store.size()-1);
+    if(log2n & (log2n-1)) {
+        // log2n is not a power of 2, round it up to the nearest power of 2.
+        segment_size = (int)floor(log2(1<<(log2n+1)));
+    }
+    else {
+        // log2n is a power of 2, so, all is fine.
+        segment_size = log2n;
+    }
+    l = log2n - log2(segment_size);
+
+    // Now assert that the upper thresholds are sane, and you do not go out of balance the very first time.
+#ifndef OPTIMIZE
+    assert(!is_too_full());
+#endif
+    total_moves = 0;
+    // And we have set this thing in motion. Pray!
+}
+
+template <class E>
+PackedMemoryArray<E>::~PackedMemoryArray() {
+}
+
+template <class E>
+void PackedMemoryArray<E>::print() const {
+    int empty = 0;
+    for (int i = 0; i < store_size(); i++) {
+        if(!ELEM_EXISTS_AT(i)) 
+            std::cerr << "-- ", empty++;
+        else
+            std::cerr << store[i] << " ";
+    }
+    std::cerr << std::endl;
+    std::cerr << empty << "/" << store.size() << std::endl;
+}
+
+template <class E>
+inline void PackedMemoryArray<E>::insert_element_at(E e, int index) {
+    // There is no element at index 'index'
+#ifndef OPTIMIZE
+    assert(!ELEM_EXISTS_AT(index));
+#endif
+    // Actually putting the element
+    store[index] = e;
+    // Marking the entry in the bitmask
+    exists[index] = 1;
+    // The bitmask works fine
+#ifndef OPTIMIZE
+    assert(ELEM_EXISTS_AT(index));
+#endif
+    // Increase the size
+    ++s;
+}
+
+template <class E>
+int PackedMemoryArray<E>::find(E e) const {
+    // TODO Make this binary search
+    for(int i = 0; i < store.size(); i++) {
+        if(ELEM_EXISTS_AT(i)) {
+            if(store[i] == e)
+                return i;
+            else if(store[i] > e)
+                return -1;
+        }
+    }
+    return -1;
+}
+
+template <class E>
+void PackedMemoryArray<E>::insert_element_after(E e, E after, int pos) {
+    // Find where we can insert
+    int loc;
+    loc = pos;
+#ifndef OPTIMIZE
+    assert(loc != -1);
+#endif
+    int insert_at = ++loc;
+    // Do we have space at the location we want to insert?
+    if(insert_at < (int)store.size() && !ELEM_EXISTS_AT(insert_at)) {
+        // Great! Now insert it there.
+        insert_element_at(e, insert_at);
+        return;
+    }
+    // The not so nice part begins here.
+    int node_index, node_level;
+    if(smallest_interval_in_balance(insert_at, &node_index, &node_level) == -1) {
+        // No more space left in the PMA. Resize!
+        expand_PMA(e);
+    }
+    else {
+        // Rebalance one particular level
+        rebalance(node_index, node_level, e);
+    }
+}
+
+template <class E>
+int PackedMemoryArray<E>::upper_bound_in_segment(E e, int v) {
+    int best = -1;
+    for(int i = v*segment_size; i < (v+1)*segment_size; i++)
+        if(ELEM_EXISTS_AT(i) && store[i] <= e) 
+            return best = i;
+    return best;
+}
+
+template <class E>
+int PackedMemoryArray<E>::upper_bound(E e) {
+    int l = 0, r = ((int)store.size())/segment_size, pos; 
+    while(l != r) {
+        int m = l + (r - l + 1)/2;
+        pos = upper_bound_in_segment(e, m);
+        if (pos == -1) 
+            r = m-1;
+        else
+            l = m; 
+    }
+    pos = upper_bound_in_segment(e, l);
+    return pos;
+}
+
+template <class E>
+inline void PackedMemoryArray<E>::insert_element(E e) {
+    int pos = upper_bound(e);
+    insert_element_after(e, store[pos], pos);
+}
+
+template <class E>
+int PackedMemoryArray<E>::smallest_interval_in_balance(int index, int * node_index, int * node_level) const {
+    // If we are trying to insert at the end of the PMA
+    if (index == (int)store.size()) {
+        index = (int)store.size() - 1;
+    }
+
+    int level = -1;
+    int start = index;
+    int end = index, count = 1;
+    unsigned int sz = segment_size;
+    bool found = false;
+    do {
+        // Get the boundaries of the next interval
+        int left = start - (start % sz);
+        int right = left + sz;
+
+        // Count only the necessary parts
+        for(int i = left; i < start; i++)
+            if(ELEM_EXISTS_AT(i))
+                count++;
+        for(int i = end + 1; i <= right; i++)
+            if(ELEM_EXISTS_AT(i))
+                count++;
+        
+        start = left;
+        end = right;
+
+        ++level;
+        bool is_balanced = !is_out_of_balance(count + 1, level);
+        // std::cout << "Level: " << level << ", from " << left << " to " << right << ", having " << count+1 << ", elements, is balanced?: " << is_balanced << ", segment_size: " << smallest_window_size << std::endl;
+        // Would be able to fit another element?
+        if(is_balanced) {
+            found = true;
+            break;
+        }
+        sz <<= 1;
+        
+    } while(sz <= store.size());
+    if(!found) {
+        // We did not find a balanced interval
+        *node_index = *node_level = -1;
+        return -1;
+    }
+
+    *node_index = start;
+    *node_level = level;
+    return 1;
+}
+
+template <class E>
+void PackedMemoryArray<E>::expand_PMA(E e) {
+    // Create a new store
+    std::vector<E> new_store;
+    new_store.resize(store.size() * 2);
+    std::vector<bool> new_exists;
+    new_exists.resize((int)ceil(s+1));
+    
+    int count = 0, i;
+    // Insert all elements less than e
+    for(i = 0; i < (int)store.size(); i++) 
+        if(ELEM_EXISTS_AT(i)) {
+            if(store[i] > e)
+                break;
+            new_exists[count] = 1;
+            new_store[count++] = store[i];
+            total_moves++;
+        }
+    
+    // Insert the element we wanted
+    new_exists[count] = 1;
+    new_store[count++] = e;
+    total_moves++;
+    
+    // Insert rest of the elements
+    for(; i < (int)store.size(); i++) 
+        if(ELEM_EXISTS_AT(i)) {
+            new_exists[count] = 1;
+            new_store[count++] = store[i];
+            total_moves++;
+        }
+
+    // Replace the existing store and bitmask
+    store = new_store;
+    exists = new_exists;
+ 
+    // Increment the number of elements in the PMA
+    s++;
+    
+    // std::cout << "Old smallest window size: " << segment_size << std::endl;
+    // Recalculate l and segment_size
+    int log2n = __builtin_popcount(store.size()-1);
+    if(log2n & (log2n-1)) {
+        // log2n is not a power of 2, round it up to the nearest power of 2.
+        segment_size = 1<<((int)floor(log2(log2n<<1)));
+    }
+    else {
+        // log2n is a power of 2, so, all is fine.
+        segment_size = 1<<log2n;
+    }
+    l = log2n - log2(segment_size);
+    // std::cout << "New smallest window size: " << segment_size << std::endl;
+
+    // Now rebalance the entire PMA 
+    rebalance(0, l);
+}
+
+template<class E>
+void PackedMemoryArray<E>::rebalance(int index, int level, E e) {
+#ifndef OPTIMZE
+    assert(level <= l);
+#endif
+    int c = CAPACITY_AT(level);
+    // Move all the elements to one side
+    int last = index + c - 1, count = 0;
+    bool element_inserted = false;
+    std::vector<E> level_copy;
+    for(int i = last; i >= index; i--) {
+        if(ELEM_EXISTS_AT(i)) {
+            if(!element_inserted && store[i] < e) {
+                level_copy.push_back(e);
+                element_inserted = true;
+                total_moves++;
+            }
+            level_copy.push_back(store[i]);
+            delete_element_at(i);
+            total_moves++;
+            --last;
+            count++;
+        }
+    }
+
+    if(!element_inserted) {
+        level_copy.push_back(e);
+        total_moves++;
+    }
+
+    // Now copy
+    double k = (c*1.0)/(level_copy.size()), p = 0;
+    int correct_index;
+    for(int i = level_copy.size()-1; i >= 0; i--) {
+        p += k;
+        // Now insert the element at the right position
+        correct_index = index + (int)p - 1;
+        insert_element_at(level_copy[i], correct_index);
+    } 
+}
+
+
+template<class E>
+void PackedMemoryArray<E>::rebalance(int index, int level) {
+#ifndef OPTIMIZE 
+    assert(level <= l);
+#endif
+    int c = CAPACITY_AT(level);
+    // Move all the elements to one side
+    int last = index + c - 1, count = 0;
+    for(int i = last; i >= index; i--) {
+        if(ELEM_EXISTS_AT(i)) {
+            if(i != last) {
+                // Copy the element to the leftmost position
+                insert_element_at(store[i], last);
+                // Delete the original copy of the element
+                #ifndef OPTIMIZE
+                    delete_element_at(i);
+                #else
+                    exists[i] = 0;
+                #endif
+               // Update the leftmost pointer, and count of elements moved
+            }
+            --last;
+            count++;
+        }
+    }
+
+    // Now copy
+    double k = (c*1.0)/count, p = 0;
+    int actual_index = last, correct_index;
+    for(int i = 0; i < count; i++) {
+        p += k;
+        actual_index++;
+        // Now insert the element at the right position
+        correct_index = index + (int)p - 1;
+        if (correct_index == actual_index)
+            continue;
+        if(actual_index != correct_index)
+            insert_element_at(store[actual_index], correct_index);
+        // Remove the left most copy
+#ifndef OPTIMIZE
+        delete_element_at(actual_index);
+#else
+        exists[actual_index] = 0;
+#endif
+    } 
+}
+
+template <class E>
+void PackedMemoryArray<E>::delete_element_at(int index) {
+#ifndef OPTIMIZE
+    assert(ELEM_EXISTS_AT(index));
+#endif
+    // Just mark it non existent
+    exists[index] = 0;
+}
+
+int main() {
+    
+    PackedMemoryArray<int> pma(2);
+    
+    Timer t;
+    t.start();
+    for(int i = 3; i < 100; i++) {
+        pma.insert_element(i);
+    }
+    double time_taken = t.stop();
+    std::cout << "Head Inserts: " << time_taken/1000000.0 << " seconds " << std::endl;
+    std::cout << "Elements Moved: " << pma.total_moves << std::endl;
+}
+
---
+++ pma_tests/impl2.cpp
@@ -0,0 +1,396 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "../include/timer.hpp"
+
+using namespace std;
+
+// #define dprintf(args...) printf(args)
+#define dprintf(args...)
+
+typedef vector<int> vi_t;
+
+int log2(int n) {
+    int lg2 = 0;
+    while (n > 1) {
+        n /= 2;
+        ++lg2;
+    }
+    return lg2;
+}
+
+unsigned int nmoves = 0;
+
+struct PMA {
+    vi_t impl;
+    int nelems;
+    vector<bool> present;
+    int chunk_size;
+    int nchunks;
+    int nlevels;
+    int lgn;
+    vi_t tmp;
+
+    struct PMAIterator {
+        PMA *pma;
+        int i;
+
+        PMAIterator(PMA *p, int _i)
+            : pma(p), i(_i)
+        { }
+
+        PMAIterator(const PMAIterator &rhs) {
+            this->pma = rhs.pma;
+            this->i   = rhs.i;
+        }
+
+        PMAIterator&
+        operator=(PMAIterator &rhs) {
+            this->pma = rhs.pma;
+            this->i   = rhs.i;
+            return *this;
+        }
+
+        PMAIterator&
+        operator++() {
+            if (i < (int)pma->impl.size()) ++i;
+            while (i < (int)pma->impl.size() && !pma->present[i]) {
+                ++i;
+            }
+            return *this;
+        }
+
+        PMAIterator
+        operator++(int) {
+            PMAIterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+
+        bool
+        operator==(PMAIterator rhs) {
+            return this->pma == rhs.pma && this->i == rhs.i;
+        }
+
+        bool
+        operator!=(PMAIterator rhs) {
+            return !(*this == rhs);
+        }
+
+        int&
+        operator*() {
+            assert(pma->present[this->i]);
+            return pma->impl[this->i];
+        }
+
+        int*
+        operator->() {
+            assert(pma->present[this->i]);
+            return &(pma->impl[this->i]);
+        }
+    };
+
+    typedef PMAIterator iterator;
+
+    PMA(int capacity = 2)
+        : nelems(0) {
+        assert(capacity > 1);
+        assert(1 << log2(capacity) == capacity);
+
+        this->init_vars(capacity);
+        this->impl.resize(capacity);
+        this->present.resize(capacity);
+    }
+
+
+    double
+    upper_threshold_at(int level) const {
+        assert(level <= this->nlevels);
+        double threshold = 1.0 - ((1.0 - 0.5) * level) / (double)this->lgn;
+        return threshold;
+    }
+
+    void
+    init_vars(int capacity) {
+        this->chunk_size = 1 << log2(log2(capacity) * 2);
+        assert(this->chunk_size == (1 << log2(this->chunk_size)));
+        this->nchunks = capacity / this->chunk_size;
+        this->nlevels = log2(this->nchunks);
+        this->lgn = log2(capacity);
+        dprintf("init_vars::capacity: %d, nelems: %d, chunk_size: %d, nchunks: %d\n", capacity, nelems, chunk_size, nchunks);
+    }
+
+    int
+    left_interval_boundary(int i, int interval_size) {
+        assert(interval_size == (1 << log2(interval_size)));
+        assert(i < (int)this->impl.size());
+
+        int q = i / interval_size;
+        int boundary = q * interval_size;
+        dprintf("left_interval_boundary(%d, %d) = %d\n", i, interval_size, boundary);
+        return boundary;
+    }
+
+    void
+    resize(int capacity) {
+        assert(capacity > this->impl.size());
+        assert(1 << log2(capacity) == capacity);
+
+        vi_t tmpi(capacity);
+        vector<bool> tmpp(capacity);
+        double d = (double)capacity / this->nelems;
+        int ctr = 0;
+        for (int i = 0; i < (int)this->impl.size(); ++i) {
+            if (this->present[i]) {
+                int idx = d*(ctr++);
+                tmpp[idx] = true;
+                tmpi[idx] = this->impl[i];
+            }
+        }
+        this->impl.swap(tmpi);
+        this->present.swap(tmpp);
+        this->init_vars(capacity);
+        nmoves += this->impl.size();
+        // dprintf("After resize: ");
+        // this->print();
+    }
+
+    void
+    get_interval_stats(int left, int level, bool &in_limit, int &sz) {
+        double t = upper_threshold_at(level);
+        int w = (1 << level) * this->chunk_size;
+        sz = 0;
+        for (int i = left; i < left + w; ++i) {
+            sz += this->present[i] ? 1 : 0;
+        }
+        double q = (double)sz / double(w);
+        dprintf("q: %f, t: %f\n", q, t);
+        in_limit = q < t;
+    }
+
+    int
+    lb_in_chunk(int l, int v) {
+        int i;
+        for (i = l; i < l + chunk_size; ++i) {
+            if (this->present[i]) {
+                if (this->impl[i] >= v) {
+                    return i;
+                }
+            }
+        }
+        return i;
+    }
+
+    int
+    lower_bound(int v) {
+        int i;
+        if (this->nelems == 0) {
+            i = this->impl.size();
+        } else {
+#if 0
+            for (i = 0; i < this->impl.size(); ++i) {
+                if (this->present[i] && !(this->impl[i] < v)) {
+                    break;
+                }
+            }
+#else
+            int l = 0, r = this->nchunks;
+            int m;
+            while (l != r) {
+                m = l + (r-l)/2;
+                int sz;
+                int left = left_interval_boundary(m * chunk_size, chunk_size);
+                int pos = lb_in_chunk(left, v);
+
+                // Why does this work? We assume that every chunk of
+                // size this->chunk_size contains at least 1
+                // element. Hence, if we reach the end of an interval
+                // without finding a lower bound, we conclude that all
+                // the elements in this chunk are < 'v'. Because every
+                // chunk contains at least 1 element, we will never
+                // reach the end of an interval because the interval
+                // is empty.
+                //
+                // Note: This is why we need lower density thresholds!
+                // 
+                if (pos == left + chunk_size) {
+                    // Move to right half
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+            i = l * chunk_size;
+#endif
+        }
+        dprintf("lower_bound(%d) == %d\n", v, i);
+        return i;
+    }
+
+    void
+    insert_merge(int l, int v) {
+        dprintf("insert_merge(%d, %d)\n", l, v);
+        // Insert by merging elements in a window of size 'chunk_size'
+        tmp.clear();
+        tmp.reserve(this->chunk_size);
+        for (int i = l; i < l + this->chunk_size; ++i) {
+            if (this->present[i]) {
+                this->present[i] = false;
+                tmp.push_back(this->impl[i]);
+            }
+        }
+        vi_t::iterator iter = std::lower_bound(tmp.begin(), tmp.end(), v);
+        tmp.insert(iter, v);
+
+        dprintf("insert_merge::tmp.size(): %d\n", tmp.size());
+        for (int i = 0; i < tmp.size(); ++i) {
+            this->present[l + i] = true;
+            this->impl[l + i] = tmp[i];
+        }
+        ++this->nelems;
+        nmoves += chunk_size;
+    }
+
+    void
+    rebalance_interval(int left, int level) {
+        dprintf("rebalance_interval(%d, %d)\n", left, level);
+        int w = (1 << level) * this->chunk_size;
+        tmp.clear();
+        tmp.reserve(w);
+        for (int i = left; i < left + w; ++i) {
+            if (this->present[i]) {
+                tmp.push_back(this->impl[i]);
+                this->present[i] = false;
+            }
+        }
+        double m = (double)(1<<level)*chunk_size / (double)tmp.size();
+        dprintf("m: %f, tmp.size(): %d\n", m, tmp.size());
+        assert(m >= 1.0);
+        for (int i = 0; i < tmp.size(); ++i) {
+            int k = i * m + left;
+            if (k >= left + w) {
+                dprintf("k: %d, left+w: %d\n", k, left + w);
+            }
+            assert(k < left + w);
+            this->present[k] = true;
+            this->impl[k] = tmp[i];
+        }
+        nmoves += w;
+    }
+
+    void
+    insert(int v) {
+        /*
+        if ((this->nelems + 2) * 2 > this->impl.size()) {
+            // resize array
+            this->resize(2 * this->impl.size());
+        }
+        */
+
+        int i = lower_bound(v);
+        if (i == this->impl.size()) {
+            --i;
+        }
+        assert(i > -1);
+        assert(i < this->impl.size());
+
+        // Check in a window of size 'w'
+        int w = chunk_size;
+        int level = 0;
+        int l = this->left_interval_boundary(i, w);
+
+        // Number of elements in current window. We just need sz to be
+        // less than w -- we don't need the exact value of 'sz' here.
+        int sz = w - 1;
+
+        bool in_limit = false;
+
+        // If the current chunk has space, then the last element will
+        // be unused (with significant probability). First check that
+        // as a quick check.
+        if (this->present[l + this->chunk_size - 1]) {
+            get_interval_stats(l, level, in_limit, sz);
+        }
+
+        if (sz < w) {
+            // There is some space in this interval. We can just
+            // shuffle elements and insert.
+            this->insert_merge(l, v);
+        } else {
+            // No space in this interval. Find an interval above this
+            // interval that is within limits, re-balance, and
+            // re-start insertion.
+            in_limit = false;
+            while (!in_limit) {
+                w *= 2;
+                level += 1;
+                // assert(level <= this->nlevels);
+                if (level > this->nlevels) {
+                    // Root node is out of balance. Resize array.
+                    this->resize(2 * this->impl.size());
+                    this->insert(v);
+                    return;
+                }
+
+                l = this->left_interval_boundary(i, w);
+                get_interval_stats(l, level, in_limit, sz);
+                dprintf("level: %d, this->nlevels: %d, in_limit: %d, sz: %d\n", level, this->nlevels, in_limit, sz);
+            }
+            this->rebalance_interval(l, level);
+            this->insert(v);
+        }
+
+    } // insert(int v)
+
+    int
+    size() const {
+        return this->nelems;
+    }
+
+    iterator
+    begin() {
+        return iterator(this, 0);
+    }
+
+    iterator
+    end() {
+        return iterator(this, this->impl.size());
+    }
+
+    void
+    print() {
+        for (int i = 0; i < (int)this->impl.size(); ++i) {
+            printf("%3d ", this->present[i] ? this->impl[i] : -1);
+        }
+        printf("\n");
+    }
+
+};
+
+template <typename Iter>
+bool
+is_sorted(Iter f, Iter l) {
+    Iter next = f;
+    while (f != l) {
+        ++next;
+        if (next != l && *f > *next) {
+            return false;
+        }
+        f = next;
+    }
+    return true;
+}
+
+int
+main() {
+    PMA p1;
+    for (int i = 0; i < 100; ++i) {
+        p1.insert(10000000 - i);
+        // v.insert(v.begin(), 100000 - i);
+    }
+    //printf("Time taken: %lf\n", ms/1000000.0);
+    printf("%d moves to insert %d elements\n", nmoves, p1.size());
+}
